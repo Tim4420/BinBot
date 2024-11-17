17d0a9d47d73baded19faeb8fe87611d2c51bcd9
MikhailRGVSS <mikhail.rahmatullah@student.tdsb.on.ca>
2024-11-16 23:04:02 -0500

    removed darkmode toggle

--- webcamScript.js
+++ webcamScript.js
@@ -40,21 +40,6 @@ function captureFrame() {
     });
 }
 
-const darkModeToggle = document.getElementById("darkModeToggle");
-
-if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-}
-
-darkModeToggle.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('darkMode', 'enabled');
-    } else {
-        localStorage.setItem('darkMode', 'disabled');
-    }
-});
-
 captureButton.addEventListener('click', captureFrame);
 
 startWebcam();
