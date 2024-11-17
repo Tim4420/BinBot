fa952a89ac4e9a5cdf050a122aa3625377452a2d
MikhailRGVSS <mikhail.rahmatullah@student.tdsb.on.ca>
2024-11-16 22:51:52 -0500

    darkmode fixes

--- webcamScript.js
+++ webcamScript.js
@@ -44,13 +44,10 @@ const darkModeToggle = document.getElementById("darkModeToggle");
 
 if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
-    captureButton.classList.add('dark-mode');
 }
 
-darkModeToggle.addEventListener('click', function() {
+darkModeToggle.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-    captureButton.classList.toggle('dark-mode');
-
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
     } else {
